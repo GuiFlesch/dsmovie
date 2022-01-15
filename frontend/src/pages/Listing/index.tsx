6b74d6f64fa490ea687bd21b9d3c2947f7d53083
Guilherme Flesch <guilherme.flesch92@gmail.com>
2022-01-15 16:12:25 -0300

    teste

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,9 @@
 import Pagination from "components/Pagination"
-import MovieScore from "components/MovieScore";
 import MovieCard from "components/MovieCard";
 
 function Listing() {
+    
     return (
-
         <>
             <Pagination />
 
